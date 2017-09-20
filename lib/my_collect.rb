1faf82b434e198942d206685be07545aed8578cc
alpbourne <alpbourne@gmail.com>
2017-09-20 17:38:37 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,3 +1,8 @@
 array = ["Hot Pink", "Grape Purple", "Sky Blue", "Lime Green"]
 
 def my_collect(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << array.split(" ").last
+    
