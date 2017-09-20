1020c23c4f2041b52b2902838c96c9e2af451f1d
alpbourne <alpbourne@gmail.com>
2017-09-20 17:40:15 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,5 +4,9 @@ def my_collect(array)
   i = 0
   collection = []
   while i < array.length
-    collection << array.split(" ").last
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
     
